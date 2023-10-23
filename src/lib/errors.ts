45400884bfbef72605a94b4a1c7e3f4269bdb248
kleva-j <kasmickleva@gmail.com>
2023-10-23 14:41:18 +0100

    update error types

--- src/lib/errors.ts
+++ src/lib/errors.ts
@@ -1,4 +1,10 @@
-export class UserNotFoundError extends Error {}
-export class UserNotAuthorizedError extends Error {}
+export class NotFoundError extends Error {}
+export class NotAuthorizedError extends Error {}
+
+// User Errors
+export class UserNotFoundError extends NotFoundError {}
+export class UserNotAuthorizedError extends NotAuthorizedError {}
+
+// Form Errors
 export class FormNotFoundError extends Error {}
 export class FormSubmissionError extends Error {}
