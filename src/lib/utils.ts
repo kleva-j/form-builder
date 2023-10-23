5acc793b0dc133c506b26fab0b8421fe999a7b98
kleva-j <kasmickleva@gmail.com>
2023-10-23 12:43:30 +0100

    feat: database seeding

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { faker } from "@faker-js/faker";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function generateFormField(num: number = 1) {
+  return new Array(num).fill(null).map(() => ({
+    userId: faker.string.uuid(),
+    name: faker.lorem.words({ min: 1, max: 3 }),
+    description: faker.lorem.sentence(),
+    content: faker.lorem.sentence(),
+  }));
 }
